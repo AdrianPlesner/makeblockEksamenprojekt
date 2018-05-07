e8c1f59c3707d994371ad5b8ab108a1de3d3f91e
AdrianPlesner <33195961+AdrianPlesner@users.noreply.github.com>
2018-05-07 13:30:54 +0200

    Add files via upload

---
+++ timertest.ino
@@ -0,0 +1,25 @@
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  TCCR3A = 0;
+  TCCR3B = 0;
+  TCNT3 = 0;
+  OCR3A = 62499;
+  TCCR3A |= (1 << WGM32);
+  TCCR3B |= (1 << CS32);
+  TIMSK3 |= (1 << OCIE3A);
+
+}
+
+
+ISR(TIMER3_COMPA_vect)
+{
+  Serial.println("Timer");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("No timer");
+  delay(100);
+
+}
